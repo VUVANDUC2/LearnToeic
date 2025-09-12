eaaa7527d528e052fcff6d45f34b5c76f1e82172
VUVANDUC2 <vanduc6122003@gmail.com>
2025-09-12 20:36:32 +0700

    Initial commit

---
+++ src/main/java/LearnToeic/LearnToeicApplication.java
@@ -0,0 +1,13 @@
+package LearnToeic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnToeicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnToeicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LearnToeic/LearnToeicApplicationTests.java
@@ -0,0 +1,13 @@
+package LearnToeic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnToeicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
