f802eeca7f4539d693c6e8e9dadee6bbc32532d3
Jucky901 <truongvuon235@gmail.com>
2025-09-13 23:41:23 +0700

    simple UI for test and blog admin

---
+++ src/main/java/LearnToeic/controller/AdminController.java
@@ -0,0 +1,34 @@
+package LearnToeic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("pageTitle", "Quản trị");
+        return "admin"; 
+    }
+
+    @GetMapping("/admin/test")
+    public String test(Model model) {
+        model.addAttribute("pageTitle", "Quản trị");
+        return "admin/test"; 
+    }
+
+    @GetMapping("/admin/blog")
+    public String blog(Model model) {
+        model.addAttribute("pageTitle", "Quản trị");
+        return "admin/blog"; 
+    }
+
+    @GetMapping("/admin/document")
+    public String document(Model model) {
+        model.addAttribute("pageTitle", "Quản trị");
+        return "admin/document"; 
+    }
+}
+
+
