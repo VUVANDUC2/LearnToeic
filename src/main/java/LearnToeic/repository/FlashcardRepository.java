d9c1ecdd879bc85d0e2d1028fb4a0862461adf4d
Jucky901 <truongvuon235@gmail.com>
2025-12-07 10:28:18 +0700

    Finish flashcards

--- src/main/java/LearnToeic/repository/FlashcardRepository.java
+++ src/main/java/LearnToeic/repository/FlashcardRepository.java
@@ -1,12 +1,18 @@
 package LearnToeic.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import LearnToeic.entity.Flashcard;
 import org.springframework.stereotype.Repository;
-import java.util.List; 
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.swing.text.html.Option; 
 
 @Repository
 public interface FlashcardRepository extends JpaRepository<Flashcard, Integer> {
@@ -21,4 +27,13 @@ public interface FlashcardRepository extends JpaRepository<Flashcard, Integer> {
 
     @Query("Select f from Flashcard f WHERE f.deck = :deckName AND f.user.userId = :userId")
     List<Flashcard> findByDeckNameAndId(@Param("userId") Integer userId, @Param("deckName") String deckName);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Flashcard f WHERE f.deck = :deckName AND f.user.userId = :userId")
+    void deleteFlashcardByDeckName(@Param("userId") Integer userId,
+                                   @Param("deckName") String deckName);
+    @Query("SELECT f FROM Flashcard f WHERE f.flashcardId = :flashcardId AND f.user.userId = :userId")
+    Optional<Flashcard> findById_UserId(@Param("flashcardId") int flashcardId,
+                                         @Param("userId") int userId);
 }
